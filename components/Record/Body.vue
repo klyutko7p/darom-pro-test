d22868142899e9c8e23c93ed38d1c9387ee77572
Daniil Klyutko <klyutko.d@gmail.com>
2024-02-15 17:09:25 +0600

    add logic for deliveredPVZ

--- components/Record/Body.vue
+++ components/Record/Body.vue
@@ -21,16 +21,20 @@ const alreadyCalled = ref(false);
 onMounted(async () => {
   if (props.user.visiblePVZ === 'ВСЕ' || props.user.username === 'ОПТ') {
     toast.success('Нужный доступ подтвержден')
-    
+
   } else if (props.user.visiblePVZ !== props.row.dispatchPVZ) {
     toast.error("Это товар не Вашего ПВЗ!")
     router.push('/spreadsheets/our-ransom')
-  } 
+  }
 
   if (!alreadyCalled.value && props.user.role === 'PVZ') {
-    await updateDeliveryRow(props.row, "PVZ");
-    toast.success('Товар принят на ПВЗ!');
-    alreadyCalled.value = true;
+    if (props.row.deliveredPVZ === null) {
+      await updateDeliveryRow(props.row, "PVZ");
+      toast.success('Товар принят на ПВЗ!');
+      alreadyCalled.value = true;
+    } else {
+      toast.warning('Товар принят ранее!');
+    }
   }
 
 })
@@ -62,28 +66,25 @@ function formatPhoneNumber(phoneNumber: string) {
   <div class="mt-5 text-lg grid grid-cols-2 max-lg:flex max-lg:flex-col gap-5">
     <div class="grid grid-cols-2 border-2 max-lg:order-1 border-black p-3 border-dashed text-center">
       <h1>Телефон:</h1>
-      <h1 v-if="link?.includes('1') || link?.includes('old') && row.fromName "
+      <h1 v-if="link?.includes('1') || link?.includes('old') && row.fromName"
         class="cursor-pointer underline text-secondary-color duration-200 hover:opacity-50"
         @click="router.push(`/spreadsheets/order/${row.clientLink1}`)">
         {{ formatPhoneNumber(row.fromName) }}
       </h1>
-      <h1
-        v-if="link?.includes(2) && row.fromName"
+      <h1 v-if="link?.includes(2) && row.fromName"
         class="cursor-pointer underline text-secondary-color duration-200 hover:opacity-50"
-        @click="router.push(`/spreadsheets/order/${row.clientLink2}`)"
-      >
+        @click="router.push(`/spreadsheets/order/${row.clientLink2}`)">
         {{ row.fromName }}
       </h1>
-      <h1
-        v-if="link?.includes(3) && row.fromName"
+      <h1 v-if="link?.includes(3) && row.fromName"
         class="cursor-pointer underline text-secondary-color duration-200 hover:opacity-50"
-        @click="router.push(`/spreadsheets/order/${row.clientLink3}`)"
-      >
+        @click="router.push(`/spreadsheets/order/${row.clientLink3}`)">
         {{ row.fromName }}
       </h1>
     </div>
 
-    <div v-if="link?.includes('1') || link?.includes('old')" class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center max-lg:order-4">
+    <div v-if="link?.includes('1') || link?.includes('old')"
+      class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center max-lg:order-4">
       <h1>Доставлено на СЦ:</h1>
       <Icon @click="updateDeliveryRow(row, 'SC')" v-if="!row.deliveredSC &&
         user.dataOurRansom === 'WRITE' &&
@@ -97,17 +98,11 @@ function formatPhoneNumber(phoneNumber: string) {
 
     <div v-if="link?.includes(2)" class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center">
       <h1>Доставлено на СЦ:</h1>
-      <Icon
-        @click="updateDeliveryRow(row, 'SC')"
-        v-if="
-          !row.deliveredSC &&
-          user.dataClientRansom === 'WRITE' &&
-          user.deliveredSC2 === 'WRITE'
-        "
-        class="text-green-500 mx-auto cursor-pointer hover:text-green-300 duration-200"
-        name="mdi:checkbox-multiple-marked-circle"
-        size="32"
-      />
+      <Icon @click="updateDeliveryRow(row, 'SC')" v-if="!row.deliveredSC &&
+        user.dataClientRansom === 'WRITE' &&
+        user.deliveredSC2 === 'WRITE'
+        " class="text-green-500 mx-auto cursor-pointer hover:text-green-300 duration-200"
+        name="mdi:checkbox-multiple-marked-circle" size="32" />
       <h1 class="font-bold text-green-500">
         {{ row.deliveredSC ? storeUsers.getNormalizedDate(row.deliveredSC) : "" }}
       </h1>
@@ -115,17 +110,11 @@ function formatPhoneNumber(phoneNumber: string) {
 
     <div v-if="link?.includes(3)" class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center">
       <h1>Отсортировано:</h1>
-      <Icon
-        @click="updateDeliveryRow(row, 'sorted')"
-        v-if="
-          !row.sorted &&
-          user.dataDelivery === 'WRITE' &&
-          user.sorted === 'WRITE'
-        "
-        class="text-green-500 mx-auto cursor-pointer hover:text-green-300 duration-200"
-        name="mdi:checkbox-multiple-marked-circle"
-        size="32"
-      />
+      <Icon @click="updateDeliveryRow(row, 'sorted')" v-if="!row.sorted &&
+        user.dataDelivery === 'WRITE' &&
+        user.sorted === 'WRITE'
+        " class="text-green-500 mx-auto cursor-pointer hover:text-green-300 duration-200"
+        name="mdi:checkbox-multiple-marked-circle" size="32" />
       <h1 class="font-bold text-green-500">
         {{ row.sorted ? storeUsers.getNormalizedDate(row.sorted) : "" }}
       </h1>
@@ -136,7 +125,8 @@ function formatPhoneNumber(phoneNumber: string) {
       <h1>{{ row.cell }}</h1>
     </div>
 
-    <div v-if="link?.includes('1') || link?.includes('old')" class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center max-lg:order-5">
+    <div v-if="link?.includes('1') || link?.includes('old')"
+      class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center max-lg:order-5">
       <h1>Доставлено на ПВЗ:</h1>
       <Icon @click="updateDeliveryRow(row, 'PVZ')" v-if="!row.deliveredPVZ &&
         user.dataOurRansom === 'WRITE' &&
@@ -150,22 +140,16 @@ function formatPhoneNumber(phoneNumber: string) {
 
     <div v-if="link?.includes(2)" class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center">
       <h1>Доставлено на ПВЗ:</h1>
-      <Icon
-        @click="updateDeliveryRow(row, 'PVZ')"
-        v-if="
-          !row.deliveredPVZ &&
-          user.dataClientRansom === 'WRITE' &&
-          user.deliveredPVZ2 === 'WRITE'
-        "
-        class="text-green-500 mx-auto cursor-pointer hover:text-green-300 duration-200"
-        name="mdi:checkbox-multiple-marked-circle"
-        size="32"
-      />
+      <Icon @click="updateDeliveryRow(row, 'PVZ')" v-if="!row.deliveredPVZ &&
+        user.dataClientRansom === 'WRITE' &&
+        user.deliveredPVZ2 === 'WRITE'
+        " class="text-green-500 mx-auto cursor-pointer hover:text-green-300 duration-200"
+        name="mdi:checkbox-multiple-marked-circle" size="32" />
       <h1 class="font-bold text-green-500">
         {{ row.deliveredPVZ ? storeUsers.getNormalizedDate(row.deliveredPVZ) : "" }}
       </h1>
     </div>
-    
+
     <div v-if="row.nameOfAction" class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center">
       <h1>Название:</h1>
       <h1>{{ row.nameOfAction }}</h1>
@@ -173,17 +157,11 @@ function formatPhoneNumber(phoneNumber: string) {
 
     <div v-if="link?.includes(3)" class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center">
       <h1>Оплачено:</h1>
-      <Icon
-        @click="updateDeliveryRow(row, 'paid')"
-        v-if="
-          !row.paid &&
-          user.dataDelivery === 'WRITE' &&
-          user.paid === 'WRITE'
-        "
-        class="text-green-500 mx-auto cursor-pointer hover:text-green-300 duration-200"
-        name="mdi:checkbox-multiple-marked-circle"
-        size="32"
-      />
+      <Icon @click="updateDeliveryRow(row, 'paid')" v-if="!row.paid &&
+        user.dataDelivery === 'WRITE' &&
+        user.paid === 'WRITE'
+        " class="text-green-500 mx-auto cursor-pointer hover:text-green-300 duration-200"
+        name="mdi:checkbox-multiple-marked-circle" size="32" />
       <h1 class="font-bold text-green-500">
         {{ row.paid ? storeUsers.getNormalizedDate(row.paid) : "" }}
       </h1>
@@ -197,7 +175,8 @@ function formatPhoneNumber(phoneNumber: string) {
     </div>
 
 
-    <div v-if="link?.includes('1') || link?.includes('old')" class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center max-lg:order-6">
+    <div v-if="link?.includes('1') || link?.includes('old')"
+      class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center max-lg:order-6">
       <h1>Выдан клиенту:</h1>
       <Icon @click="updateDeliveryRow(row, 'issued')"
         v-if="!row.issued && user.dataOurRansom === 'WRITE' && user.issued1 === 'WRITE'"
@@ -210,13 +189,10 @@ function formatPhoneNumber(phoneNumber: string) {
 
     <div v-if="link?.includes(2)" class="grid grid-cols-2 border-2 border-black p-3 border-dashed text-center">
       <h1>Выдан клиенту:</h1>
-      <Icon
-        @click="updateDeliveryRow(row, 'issued')"
+      <Icon @click="updateDeliveryRow(row, 'issued')"
         v-if="!row.issued && user.dataClientRansom === 'WRITE' && user.issued2 === 'WRITE'"
         class="text-green-500 mx-auto cursor-pointer hover:text-green-300 duration-200"
-        name="mdi:checkbox-multiple-marked-circle"
-        size="32"
-      />
+        name="mdi:checkbox-multiple-marked-circle" size="32" />
       <h1 class="font-bold text-green-500">
         {{ row.issued ? storeUsers.getNormalizedDate(row.issued) : "" }}
       </h1>
