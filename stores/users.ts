8cfdf1332c0c3e7c74b875b7fa27fa371247fea6
Daniil Klyutko <klyutko.d@gmail.com>
2024-03-02 02:13:24 +0600

    remove filters after exit

--- stores/users.ts
+++ stores/users.ts
@@ -57,6 +57,7 @@ export const useUsersStore = defineStore("users", () => {
     async function signOut() {
         const cookies = Object.keys(Cookies.get());
         cookies.forEach(cookie => Cookies.remove(cookie));
+        localStorage.clear();
         userData = {} as User;
         router.push('/auth/login');
     }
