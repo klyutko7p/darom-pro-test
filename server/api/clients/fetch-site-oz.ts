e17e97317d0dd1b85d2565e46626ca97666b2d7e
Daniil Klyutko <klyutko.d@gmail.com>
2024-04-20 15:56:32 +0600

    bug fix

--- server/api/clients/fetch-site-oz.ts
+++ server/api/clients/fetch-site-oz.ts
@@ -10,12 +10,11 @@ export default defineEventHandler(async (event) => {
     let productId = link.split("/");
 
     let apiUrl =
-      "https://www.ozon.ru/api/entrypoint-api.bx/page/json/v2?url=%2Fproduct%2F" +
+      "https://api.ozon.ru/composer-api.bx/page/json/v2?url=/product/" +
       productId[4];
 
-    let queryJson = await fetch('https://api.ozon.ru/composer-api.bx/page/json/v2?url=/product/rog-cetra-true-wireless-igry-besprovodnaya-belye-naushniki-bluetooth-907393197').then((response) => response.json());
+    let queryJson = await fetch(apiUrl).then((response) => response.json());
     return queryJson;
-    // return queryJson.seo.script[0].innerHTML;
   } catch (error) {
     if (error instanceof Error) {
       return { error: error.message };
