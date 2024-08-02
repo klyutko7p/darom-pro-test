144ca1b49f7e804423368ea07ba731e4408497e7
klyutko7p <klyutko.d@gmail.com>
2024-08-02 22:09:42 +0500

    bug fix

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,6 +5,7 @@ importScripts(
   "https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js"
 );
 
+
 const app = firebase.initializeApp({
   apiKey: "AIzaSyDXooTcaUHHUHe8Pwlmg8Ua3zwA0nWxuqw",
   authDomain: "darom-pro-messages.firebaseapp.com",
@@ -15,20 +16,51 @@ const app = firebase.initializeApp({
   measurementId: "G-GC3VTLSR3S",
 });
 
+// let messaging;
+// try {
+//   messaging = firebase.messaging();
+//   messaging.onBackgroundMessage((payload) => {
+//     console.log("Received a bg message: ", payload);
+
+//     const title = payload.notification.title;
+//     const notification = {
+//       body: "Статус заказа: Darom.pro",
+//       icon: "pwa-512x512.png",
+//     };
+
+//     self.registration.showNotification(title, notification);
+//   });
+// } catch (err) {
+//   console.error("Failed to initialize Firebase Messaging", err);
+// }
+
 let messaging;
 try {
   messaging = firebase.messaging();
-  messaging.onBackgroundMessage((payload) => {
-    console.log("Received a bg message: ", payload);
 
-    const title = payload.notification.title;
-    const notification = {
-      body: "Статус заказа: Darom.pro",
-      icon: "pwa-512x512.png",
-    };
+  if (typeof navigator.serviceWorker !== "undefined") {
+    navigator.serviceWorker.getRegistration().then((registration) => {
+      if (registration) {
+        messaging.onBackgroundMessage((payload) => {
+          console.log("Received a bg message: ", payload);
 
-    self.registration.showNotification(title, notification);
-  });
+          const title = payload.notification.title;
+          const notification = {
+            body: "Статус заказа: Darom.pro",
+            icon: "pwa-512x512.png",
+          };
+
+          registration.showNotification(title, notification);
+        });
+      } else {
+        console.warn("Service Worker registration not found.");
+      }
+    }).catch((error) => {
+      console.error("Error fetching service worker registration:", error);
+    });
+  } else {
+    console.warn("Service Worker is not supported in this browser.");
+  }
 } catch (err) {
   console.error("Failed to initialize Firebase Messaging", err);
 }
