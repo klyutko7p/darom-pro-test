170a312ab9493e9dee740e11ca12f32d65b9b1b4
klyutko7p <klyutko.d@gmail.com>
2024-08-02 21:47:51 +0500

    bug fix

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,9 @@
-importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js')
-importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js')
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js"
+);
 
 const app = firebase.initializeApp({
   apiKey: "AIzaSyDXooTcaUHHUHe8Pwlmg8Ua3zwA0nWxuqw",
@@ -9,18 +13,22 @@ const app = firebase.initializeApp({
   messagingSenderId: "633883773284",
   appId: "1:633883773284:web:5df79ceef6d300e236d271",
   measurementId: "G-GC3VTLSR3S",
-})
+});
 
-const messaging = firebase.messaging()
+let messaging;
+try {
+  messaging = firebase.messaging();
+  messaging.onBackgroundMessage((payload) => {
+    console.log("Received a bg message: ", payload);
 
-messaging.onBackgroundMessage(payload => {
-  console.log("Received a bg message: ", payload);
+    const title = payload.notification.title;
+    const notification = {
+      body: "Статус заказа: Darom.pro",
+      icon: "pwa-512x512.png",
+    };
 
-  const title = payload.notification.title
-  const notification = {
-    body: "Статус заказа: Darom.pro",
-    icon: "pwa-512x512.png"
-  }
-
-  self.registration.showNotification(title, notification);
-})
+    self.registration.showNotification(title, notification);
+  });
+} catch (err) {
+  console.error("Failed to initialize Firebase Messaging", err);
+}
