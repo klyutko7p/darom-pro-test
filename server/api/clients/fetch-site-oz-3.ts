66d4134d6fb76c3fb6c3567a90a3352f950455fa
klyutko7p <klyutko.d@gmail.com>
2024-12-06 04:57:05 +0500

    bug fix otkat 19

--- server/api/clients/fetch-site-oz-3.ts
+++ server/api/clients/fetch-site-oz-3.ts
@@ -1,32 +1,32 @@
-interface IRequestBody {
-  link: string;
-}
+// interface IRequestBody {
+//   link: string;
+// }
 
-export default defineEventHandler(async (event) => {
-  try {
-    const { link } = await readBody<IRequestBody>(event);
-    const requestData = {
-      url: link,
-    };
+// export default defineEventHandler(async (event) => {
+//   try {
+//     const { link } = await readBody<IRequestBody>(event);
+//     const requestData = {
+//       url: link,
+//     };
 
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(requestData),
-    };
+//     const options = {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(requestData),
+//     };
 
-    const response = await fetch(
-      "https://telegram-fetch-ozon-5365a314811f.herokuapp.com/parse",
-      options
-    );
-    const data = await response.json();
+//     const response = await fetch(
+//       "https://telegram-fetch-ozon-5365a314811f.herokuapp.com/parse",
+//       options
+//     );
+//     const data = await response.json();
 
-    return data;
-  } catch (error) {
-    if (error instanceof Error) {
-      return { error: error.message };
-    }
-  }
-});
+//     return data;
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       return { error: error.message };
+//     }
+//   }
+// });
