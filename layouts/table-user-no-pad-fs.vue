af17ff98c94bcbf4737bfd90848c4ee063cf8726
klyutko7p <klyutko.d@gmail.com>
2025-03-29 14:59:03 +0500

    bug fix

--- layouts/table-user-no-pad-fs.vue
+++ layouts/table-user-no-pad-fs.vue
@@ -31,7 +31,7 @@ onMounted(() => {
   <div
     class="fixed text-lg bottom-2 left-1/2 transform -translate-x-1/2 text-muted-color font-bold opacity-30 text-center max-sm:text-sm z-[100]"
   >
-    Работает на основе Trackbiz
+    Работает на Trackbis
   </div>
   <CookieBanner />
 </template>
